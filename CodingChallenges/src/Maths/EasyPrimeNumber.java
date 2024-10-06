3ec5b130ce181feee6ae33dec08c89301580d7ba
rohitsrivastava <rohitsrivastava@localhost>
2024-10-06 17:31:38 +0530

    Adding Solution for PrimeNumber

--- CodingChallenges/src/Maths/EasyPrimeNumber.java
+++ CodingChallenges/src/Maths/EasyPrimeNumber.java
@@ -4,16 +4,17 @@ public class EasyPrimeNumber {
 
 	public static void main(String[] args) {
 		// Problem Statement : To identify if the given number is prime or not
-		int n =29;
-		boolean val=true;
-		int limit = n/2;
-		for(int i=2;i<=limit;i++) {
-			if(n%i==0) {
+		// Time Complexity = O(n)
+		// Space Complexity = O(1)
+		int n = 29;
+		boolean val = true;
+		int limit = n / 2;
+		for (int i = 2; i <= limit; i++) {
+			if (n % i == 0) {
 				val = false;
 				break;
-			}
-			else {
-				val=true;
+			} else {
+				val = true;
 			}
 		}
 		System.out.println(val);
