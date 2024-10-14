88124a04b25d1772f6505ddfebe3fffbbd7f1792
rohitsrivastava <rohitsrivastava@localhost>
2024-10-15 00:51:19 +0530

    adding MoveZeroesToEnd problem

---
+++ CodingChallenges/src/Arrays/EasyMoveZerosToEnd.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class EasyMoveZerosToEnd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nums[] = { 1, 0, 3, 4, 8, 0, 9, 1, 0 };
+		int count = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				int temp = nums[i];
+				nums[i] = nums[count];
+				nums[count] = temp;
+				count++;
+			}
+		}
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+	}
+
+}
