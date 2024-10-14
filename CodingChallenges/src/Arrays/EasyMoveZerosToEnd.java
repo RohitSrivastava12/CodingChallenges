c080838a57e83e4f38bd02447728684ac899d905
rohitsrivastava <rohitsrivastava@localhost>
2024-10-15 00:51:34 +0530

    Updates

--- CodingChallenges/src/Arrays/EasyMoveZerosToEnd.java
+++ CodingChallenges/src/Arrays/EasyMoveZerosToEnd.java
@@ -4,6 +4,7 @@ public class EasyMoveZerosToEnd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Move all zeros to end
 		int nums[] = { 1, 0, 3, 4, 8, 0, 9, 1, 0 };
 		int count = 0;
 		for (int i = 0; i < nums.length; i++) {
