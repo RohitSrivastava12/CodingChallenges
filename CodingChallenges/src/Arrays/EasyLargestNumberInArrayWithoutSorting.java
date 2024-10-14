61c91aabd8bcd330d395f17aa422b67dcfd74a37
rohitsrivastava <rohitsrivastava@localhost>
2024-10-14 23:28:54 +0530

    updating largestNumberProblem

--- CodingChallenges/src/Arrays/EasyLargestNumberInArrayWithoutSorting.java
+++ CodingChallenges/src/Arrays/EasyLargestNumberInArrayWithoutSorting.java
@@ -1,16 +1,17 @@
 package Arrays;
 
 public class EasyLargestNumberInArrayWithoutSorting {
-
+//Time complexity: O(N)
+	// Space Complexity: O(1)
 	public static void main(String[] args) {
-		int arr[]= {5,4,1,2,90,98};
+		int arr[] = { 5, 4, 1, 2, 90, 98 };
 		int max = arr[0];
-		for(int i=1;i<arr.length;i++) {
-			if(arr[i]>max) {
-				max=arr[i];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
 			}
 		}
-		System.out.println("Largest Number is "+max);
+		System.out.println("Largest Number is " + max);
 
 	}
 
